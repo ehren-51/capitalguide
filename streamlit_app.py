7c6988312ed4a8ec6ea7e7276aa912e9dc2209a2
ehren-51 <160355697+ehren-51@users.noreply.github.com>
2024-07-12 17:30:56 +0000

    Personal Financial Adivsor

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,54 @@
 import streamlit as st
 from openai import OpenAI
 
-# Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
-
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+if "OPENAI_API_KEY" in st.secrets:
+        api_key = st.secrets["OPENAI_API_KEY"]
 else:
+        api_key =  st.text_input("Please enter your OpenAI API key:", type="password")
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+client = OpenAI(api_key=api_key)
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+# Show title and description.
+st.title("Personal Financial Advisor")
+age = st.text_input("How old are you?")
+# Expenses
+expenses = st.text_input("What are your expenses per month?")
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+# Income/Earnings
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+income = st.text_input("What is your annual income?")
+savings = st.text_input("How much money do you have in savings?")
+stocks = st.text_input("How much money do you already have in stocks?")
 
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+#Goal + Risk
 
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
+goalmoney = st.text_input("How much money do you want?")
+goalage = st.text_input("At what age do you want that money?")
+risk = st.text_input("How much risk are you willing to take?")
 
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+def get_financial_plan(age, expenses, income, savings, stocks, goalmoney, goalage, risk):
+    prompt = f"""I am currently {age} years old. I have monthly expenses of {expenses} dollars. My annual income is {income} dollars. \
+        I have {savings} dollars in savings and {stocks} dollars in stocks. \
+        I want to have {goalmoney} dollars when I am {goalage} years old. \
+        I am willing to take {risk} risks. If the goal is not possible in the current market, state that it isn't possible.\
+        If there is a discrepancy, if there seems to be an inaccuracy, simply state the inaccuracy, ask the user to correct it, and don't continue to respond.\
+        Provide a snapshot of my current financial situation in a list format.\
+        Create a specific financial plan including a potential portfolio allocation, profitable stock sectors with specific companies or groups,\
+        specific investment options with examples, recommended spending habits with examples, and retirement funds if applicable to the situation.\
+        When you are suggesting specific investments and investment groups, it may be helpful to use these two links\
+        https://money.usnews.com/investing/stocks/top-performers and https://money.usnews.com/funds/search. \ 
+        You should take the top 5 preforming stocks and provide information about each of them including prices and returns.\
+        Make sure to be as specific as possible in your recommendations and use numbers and percentages when helpful.\
+        Do not use an asterix or special mathematical characters that impact the format. Do not ask any additional follow up questions"""
+    response = client.chat.completions.create(model="gpt-4o",
+    messages=[
+        {"role": "system", "content": "You are a knowledgeable stock market analyst and financial advisor but can not speak on any topic that isn't related to finance. You are similar to Warren Buffet and Benjamin Graham."},
+        {"role": "user", "content": prompt}
+    ])
+    
+    return response.choices[0].message.content.strip()
+
+if st.button("Get Finacial Plan"):
+    financial_plan = get_financial_plan(age, expenses, income, savings, stocks, goalmoney, goalage, risk)
+    st.write(financial_plan)
+
